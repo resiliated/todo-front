dffedeaea33cf3572feb81dab25f5a1dd459cd6c
resiliated <101754490+resiliated@users.noreply.github.com>
2022-07-18 17:26:35 +0200

    Update index.js

--- todo-app-from-scratch/src/index.js
+++ todo-app-from-scratch/src/index.js
@@ -6,6 +6,6 @@ import App from './App.js';
 
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container); 
 
 root.render(<App/>);
