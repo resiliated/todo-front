c5ee145347adacf7e81f4c68dc102b80559c4f18
resiliated <101754490+resiliated@users.noreply.github.com>
2022-07-18 17:26:54 +0200

    Update App.js

--- todo-app-from-scratch/src/App.js
+++ todo-app-from-scratch/src/App.js
@@ -24,9 +24,6 @@ class App extends React.Component {
               todos: result
             });
           },
-          // Remarque : il est important de traiter les erreurs ici
-          // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
-          // des exceptions provenant de réels bugs du composant.
           (error) => {
             this.setState({
               isLoaded: true,
