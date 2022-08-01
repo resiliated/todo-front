b5139259befd5aaf23d54637f20ee4d5e92faa7b
resiliated <boris.raimbault.37@gmail.com>
2022-08-01 16:37:26 +0200

    Remove VPS url...

--- src/index.js
+++ src/index.js
@@ -7,6 +7,5 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <App URL_API={"http://54.37.13.50:8081/todo"}/>
-  //<App URL_API={"http://localhost:8080/todo"}/>
+  <App URL_API={"http://localhost:8080/todo"}/>
 );
